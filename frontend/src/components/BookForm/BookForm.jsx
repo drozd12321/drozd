b8f60958ce1613747b3bfa77a38f13094de73ded
Drozdov Evgeny <drozdove921@gmail.com>
2025-01-20 17:24:41 +0300

    1

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 import '../../App.css'
 import './BookForm.css'
 const BookForm = () => {
+    const [title,setTitle] = useState('')
+    const [author,setAuthors] = useState('')
+    function handleSubmit(event){
+        event.preventDefault()
+        if(title && author){
+            //dispatch
+            console.log(title,author)
+            setTitle('')
+            setAuthors('')
+        }
+
+    }
   return (
     <div className='app-block book-form'>
         <h1>Add a New Book</h1>
-        <form action="">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="title">Title:</label>
+                <input type="text"
+                id='title' 
+                value={title} 
+                onChange={(e)=>{setTitle(e.target.value);}}/>
+            </div>
+            <div >
+                <label htmlFor="author">Author:</label>
+                <input type="text"
+                id='author' 
+                value={author} 
+                onChange={(e)=>setAuthors(e.target.value)}/>
+
+            </div>
+            <button type='submit'>Add Book</button>
         </form>
     </div>
   )
