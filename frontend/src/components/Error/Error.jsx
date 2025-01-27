227181c1579cc5d53b23b4d2be5085bdeebed292
Drozdov Evgeny <drozdove912@gmail.com>
2025-01-27 09:53:07 +0300

    errorComponent

---
+++ frontend/src/components/Error/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import styles from './error.module.css'
+const Error = () => {
+  return (
+    <div>Error</div>
+  )
+}
+
+export default Error
